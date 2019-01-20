1992f0055225d85d12afd0dfa5e1a8b43de543b7
adarsh.payyakkil@gmail.com <adarsh.payyakkil@gmail.com>
2019-01-19 18:13:27 -0700

    Changed to have rotated rectangle and also ready to find equation to calculate distance

--- VisionCode/TapeDetect.py
+++ VisionCode/TapeDetect.py
@@ -16,9 +16,10 @@ class TapeDetect:
     # ###################################################################################################
     ## Process function with USB output
 	def process(self, inframe, outframe):
-		jevois.sendSerial("Hello World Process")
+		jevois.sendSerial("Hello World")
         
 	def processNoUSB(self, inframe):
+		#jevois.sendSerial("Hello World")
 		runcount = 0
 		#get image
 		inimg = inframe.getCvBGR()
@@ -29,6 +30,7 @@ class TapeDetect:
 		hsv = cv2.cvtColor(inimg, cv2.COLOR_BGR2HSV)
 		outimg = hsv
 		
+		errPass = " "
 		
 		#threshold colors to detect
 		lowerThreshold = np.array([60, 0, 0])
@@ -50,8 +52,10 @@ class TapeDetect:
 			approx = cv2.approxPolyDP(countour, 0.04 * peri, True)
 			if len(approx) == 4:
 				areas = [cv2.contourArea(c) for c in countours]
+				areas = np.sort(areas)
 				max_index = np.argmax(areas)
-				cnt=countours[max_index]
+				
+				cnt= countours[max_index]
 				rotatedRect = cv2.minAreaRect(cnt)
 				box = cv2.boxPoints(rotatedRect)
 				box = np.int0(box)
@@ -59,13 +63,13 @@ class TapeDetect:
 				lowestCoordinate = box[0]
 				
 				#cv2.rectangle(inimg,(x,y),(x+w,y+h),(0,255,0),1)
-				areas = [cv2.contourArea(c) for c in countours]
-				index2 = np.argmax(areas)
-				cnt2 = countours[index2 - 1]
+				
+				cnt2 = countours[max_index - 1]
 				rotatedRect2 = cv2.minAreaRect(cnt2)
 				box2 = cv2.boxPoints(rotatedRect2)
-				box = np.int0(box)
+				box2 = np.int0(box2)
 				angleTwo = rotatedRect2[2]
+				lowestCoordinate2 = box2[0]
 				#cv2.rectangle(inimg, (x, y), (x+w, y+h), (0,255,0), 1)
 			
 			
@@ -81,42 +85,62 @@ class TapeDetect:
 		#y = y - 120
 		
 		#determine whether we are detecting the correct contour based on if the points at the top of the contours are closer than the points at the bottoms of the contours
-		if angleOne == angleTwo:
-			yesNo = "yes"
+		#if angleOne == angleTwo:
+		#	yesNo = "yes"
+		#else:
+		#	yesNo = "no"
+		
+	
+		
+		x = lowestCoordinate[0]
+		y = lowestCoordinate[1]
+		x2 = lowestCoordinate2[0]
+		y2 = lowestCoordinate2[1]
+		centerX = x/2
+		centerY = y/2
+		
+		if x < x2:
+			leftCoordinate = lowestCoordinate
+			rightCoordinate = lowestCoordinate2
+			leftAngle = angleOne
+			rightAngle = angleTwo
 		else:
-			yesNo = "no"
+			leftCoordinate = lowestCoordinate2
+			rightCoordinate = lowestCoordinate
+			leftAngle = angleTwo
+			rightAngle = angleTwo
+			
 		
-		#bottomPointOne = 
+		distance = -0.0033508576 * y**3
+		distance = distance + 0.8429538777 * y**2
+		distance = distance - 68.64070128 * x
+		distance = distance + 1882.753427
+		distance = distance/y
 		
-		#bottomPointsDist = 
+		leftCoordinateY = leftCoordinate[1]
+		leftCoordinateX = leftCoordinate[0]
+		rightCoordinateY = rightCoordinate[1]
+		rightCoordinateX = rightCoordinate[0]
+		
 		
 		#change vals to strings to send over serial
-		#centerX = str(centerX)
-		#centerY = str(centerY)
-		#centerY2 = str(centerY2)
-		#centerX2 = str(centerX2)
-		#twoPointsDist = str(twoPointsDist)
-		#yUpPointsDist = str(yUpPointsDist)
 		angleOne = str(angleOne)
-		angleTwo = str(angleTwo)
+		#angleTwo = str(angleTwo)
 		lowestCoordinate = str(lowestCoordinate)
-		
+		x = str(x)
+		distance = str(distance)
+		centerY = str(centerY)
+		leftCoordinateY = str(leftCoordinateY)	
+			
 		#JSON not fully working yet
 		#X_Y = {"X" : centerX, "Y" : centerY, "X2" : centerX2, "Y2" : centerY2}
 		
-		#jevois.sendSerial(str(distance).format + str(Angle).format)
 		
-		#jevois.sendSerial(str(X_Y).format)
 		
 		#send vals through serial
-		#jevois.sendSerial(centerX)
-		#jevois.sendSerial(centerY)
-		#jevois.sendSerial(centerX2)
-		#jevois.sendSerial(centerY2)
-		#jevois.sendSerial(yUpPointsDist)
-		#jevois.sendSerial(twoPointsDist)
-		jevois.sendSerial(angleOne + " " + angleTwo + " " + yesNo)
-		#jevois.sendSerial(lowestCoordinate)
+
+		jevois.sendSerial(leftCoordinateY +  " " + yesNo)
+
 		
 		
 		#jevois.sendSerial(centerX, centerY, centerX2, centerY2, yUpPointsDist, twoPointsDist)
