389364122b5824a3568fbfe48507083468c94f0b
adarsh.payyakkil@gmail.com <adarsh.payyakkil@gmail.com>
2019-02-01 15:51:28 -0700

    no message

--- VisionCode/TapeDetect.py
+++ VisionCode/TapeDetect.py
@@ -56,7 +56,7 @@ class TapeDetect:
 		
 		cntArray = []
 		yesNo = " "
-		
+		pairsList = []
 		
 		#sift through contours and add 4 sided contours to cntArray
 		for countour in countours:
@@ -64,8 +64,6 @@ class TapeDetect:
 			approx = cv2.approxPolyDP(countour, 0.04 * peri, True)
 			cntArea = cv2.contourArea(countour)
 			if len(approx) == 4 and cntArea > 100 and cntArea < 800:
-				cntArea = str(cntArea)
-				jevois.sendSerial(cntArea)
 				cntArray.append(countour)
 		
 		for cnt in cntArray:
@@ -74,18 +72,30 @@ class TapeDetect:
 			boxArray = np.int0(box)
 			boxColor = (0,0,255)
 			targetAngle = rotatedRect[2]
-			
+			centerX = 0
 			if targetAngle >= -90 and targetAngle <= -55:
 				boxColor = (155, 120, 240)
+				if rotatedRect[0][0] >= centerX:
+					pairsList.append[cnt]
+					centerX = rotatedRect[0][0]
+				else:
+					boxColor = (0,0,255)
 			elif targetAngle >= -35 and targetAngle <= -0:
 				boxColor = (50, 120, 240)
+				if rotatedRect[0][0] >= centerX:
+					pairsList.append[cnt]
+					centerX = rotatedRect[0][0]
+				else:
+					boxColor = (0,0,255)
 			else:
 				cntArray.remove(cnt)
 			targetAngle = str(targetAngle)
 			centerX = str(rotatedRect[0][0])
-			jevois.sendSerial("CenterX: " + centerX + " " + "TargetAngle: " + targetAngle)
+			#jevois.sendSerial("CenterX: " + centerX + " " + "TargetAngle: " + targetAngle)
 			cv2.drawContours(outimg,[boxArray],0,boxColor,2)
 			
+			
+		
 			
 		#sort cntArray
 		sortedArray = sorted(cntArray, key=cv2.contourArea)
@@ -113,57 +123,58 @@ class TapeDetect:
 			cv2.drawContours(outimg,[secondBoxArray],0,(0, 0, 255),2)
 			
 			#center coordinates of first rectangle
-			centerY = bigBoxArray[0][1]
-			centerX = bigBoxArray[0][0]
+			#centerY = bigBoxArray[0][1]
+			#centerX = bigBoxArray[0][0]
 			#center coordinates of second rectangle
-			centerY2 = secondBoxArray[0][1]
-			centerX2 = secondBoxArray[0][0]
+			#centerY2 = secondBoxArray[0][1]
+			#centerX2 = secondBoxArray[0][0]
 			
 			#angle of robot relative to the target
-			yawAngle = (centerX - 155.5)
-			yawAngle = yawAngle * 0.203125
+			#yawAngle = (centerX - 155.5)
+			#yawAngle = yawAngle * 0.203125
 			
 			# slope (or angle) of the target
-			targetAngle_A = rotatedRect[2]
-			targetAngle_B = rotatedRect2[2]
+			#targetAngle_A = rotatedRect[2]
+			#targetAngle_B = rotatedRect2[2]
 
 			#determine which box is on the left and which is on the right
-			if centerX < centerX2:
-				leftY = centerY
-				leftX = centerX
-				rightY = centerY2
-				rightX = centerX2
-				leftAngle = targetAngle_A
-				rightAngle = targetAngle_B
-			else:
-				leftY = centerY2
-				leftX = centerX2
-				rightY = centerY
-				rightX = centerX
-				leftAngle = targetAngle_B
-				rightAngle = targetAngle_A
+			#if centerX < centerX2:
+			#	leftY = centerY
+			#	leftX = centerX
+			#	rightY = centerY2
+			#	rightX = centerX2
+			#	leftAngle = targetAngle_A
+			#	rightAngle = targetAngle_B
+			#else:
+			#	leftY = centerY2
+			#	leftX = centerX2
+			#	rightY = centerY
+			#	rightX = centerX
+			#	leftAngle = targetAngle_B
+			#	rightAngle = targetAngle_A
 			
 
+			dist = math.sqrt((leftX - rightX)**2 + (leftY - rightY)**2)
+			
 			# decide whether we are detecting the correct targets
 			#if leftAngle >= -90 and leftAngle <= -65:
 			#	if rightAngle >= -30 and rightAngle <= -5:
-			#		if dist = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
 			#		yesNo = "Yes"
 			#	else:
 			#		yesNo = "No"
 			#else:
 			#	yesNo = "No"
-			
-			
+						
 			
 			#calculate the distance to the target. The values were calculated based on trial and error
-			distance = -0.0013216275 * rightY**3 + 0.3319141337 * rightY**2 + -27.11835717 * rightY + 763.1474953
+			#distance = -0.0013216275 * rightY**3 + 0.3319141337 * rightY**2 + -27.11835717 * rightY + 763.1474953
 			
 			#change vals to string to send over serial
-			yawAngle = str(yawAngle)
-			distance = str(distance)
-			leftAngle = str(leftAngle)
-			rightAngle = str(rightAngle)
+			#yawAngle = str(yawAngle)
+			#distance = str(distance)
+			#leftAngle = str(leftAngle)
+			#rightAngle = str(rightAngle)
+			#dist = str(dist)
 			
 			#send values over serial
 			#jevois.sendSerial(yawAngle)
@@ -171,6 +182,7 @@ class TapeDetect:
 			#jevois.sendSerial(leftAngle)
 			#jevois.sendSerial(rightAngle)
 			#jevois.sendSerial(yesNo)
+
 			
 			#return an image if over usb
 		outimg = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)		
