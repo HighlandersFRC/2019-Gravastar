3d0e7a41f654b815fae86991d715f426ae898890
adarsh.payyakkil@gmail.com <adarsh.payyakkil@gmail.com>
2019-02-11 20:45:51 -0700

    Has distance, and basic outlines for detecting center target

--- VisionCode/TapeDetect.py
+++ VisionCode/TapeDetect.py
@@ -31,7 +31,6 @@ class TapeDetect:
 		out = self.UniversalProcess(inframe)
 		
 		
-		
 	def sortContours(self, cntArray):
 		arraySize = len(cntArray)
 		if arraySize == 0:
@@ -53,8 +52,23 @@ class TapeDetect:
 		
 		
 		return sortedArray
+	
+	def isLeft(self, contour):
+		rectangle = cv2.minAreaRect(contour)
+		targetAngle = rectangle[2]
+		if targetAngle >= -80 and targetAngle <= -65:
+			return True
+		else:
+			return False
 		
-		
+	def isRight(self, contour):
+		rectangle = cv2.minAreaRect(contour)
+		targetAngle = rectangle[2]
+		if targetAngle >= -25 and targetAngle <= -5:
+			return True
+		else:
+			return False
+	
 	def UniversalProcess(self, inframe):
 		#jevois.sendSerial("Hello World")
 		#jevois.sendSerial("Hello World")
@@ -68,19 +82,20 @@ class TapeDetect:
 		hsv = cv2.cvtColor(inimg, cv2.COLOR_BGR2HSV)
 		outimg = hsv
 		
-		oKernel = np.ones((13, 13), np.uint8)
-		cKernel = np.array([
-		[0, 0, 1, 0, 0],
-		[0, 1, 1, 0, 0],
-		[1, 1, 1, 1, 0],
-		[0, 1, 1, 1, 0],
-		[0, 1, 1, 1, 1],
-		[0, 0, 1, 1, 0],
-		[0, 0, 1, 0, 0]], dtype = np.uint8)
+		oKernel = np.ones((7, 7), np.uint8)
+		cKernel = np.ones((7, 7), np.uint8)
+		#cKernel = np.array([
+		#[0, 0, 1, 0, 0],
+		#[0, 1, 1, 0, 0],
+		#[1, 1, 1, 1, 0],
+		#[0, 1, 1, 1, 0],
+		#[0, 1, 1, 1, 1],
+		#[0, 0, 1, 1, 0],
+		#[0, 0, 1, 0, 0]], dtype = np.uint8)
 		
 		
 		#threshold colors to detect - Green: First value decides color, second val determines intensity, third val decides brightness
-		lowerThreshold = np.array([50, 50, 45])
+		lowerThreshold = np.array([55, 50, 20])
 		upperThreshold = np.array([85, 255, 255])
 		
 		#check if color in range
@@ -89,15 +104,17 @@ class TapeDetect:
 		#create blur on image to reduce noise
 		blur = cv2.GaussianBlur(mask,(5,5),0)
 		
+		ret,thresh = cv2.threshold(blur,180,255,0)
+		
 		#closes of noise from inside object
-		closing = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, cKernel)
+		closing = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, cKernel)
 		
 		#takes away noise from outside object
 		opening = cv2.morphologyEx(closing, cv2.MORPH_OPEN, oKernel)
 		
 		#find contours
-		#ret,thresh = cv2.threshold(closing,127,255,0)
-		countours, _ = cv2.findContours(opening, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+		
+		countours, _ = cv2.findContours(closing, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 		
 		cntArray = []
 		yesNo = " "
@@ -113,128 +130,96 @@ class TapeDetect:
 			boxArray = np.int0(box)
 			boxColor = (0,0,255)
 			if len(approx) == 4 and cntArea > 100 and cntArea < 800:
-
 				targetAngle = rotatedRect[2]
-				if targetAngle >= -80 and targetAngle <= -65:
-					cntArray.append(countour)
-					boxColor = (155, 200, 240)
-				elif targetAngle >= -25 and targetAngle <= -5:
-					cntArray.append(countour)
-					boxColor = (50, 200, 240)
-			
+				#if targetAngle >= -80 and targetAngle <= -65:
+				cntArray.append(countour)
+				#boxColor = (155, 200, 240)
+				#elif targetAngle >= -25 and targetAngle <= -5:
+				cntArray.append(countour)
+				#boxColor = (50, 200, 240)
+
 				targetAngle = str(targetAngle)
 				centerX = rotatedRect[0][0]
 				centerX = str(centerX)
-				jevois.sendSerial("CenterX: " + centerX + " " + "TargetAngle: " + targetAngle)
+				#jevois.sendSerial("CenterX: " + centerX + " " + "TargetAngle: " + targetAngle)
 				cv2.drawContours(outimg,[boxArray],0,boxColor,2)
-				#jevois.sendSerial(" ")
+		#jevois.sendSerial(" ")
 		
 		sortedArray = self.sortContours(cntArray)
 				
-		
-		for cnt in sortedArray:
-			rect = cv2.minAreaRect(cnt)
-			centerX = rect[0][0]
-			centerX = str(centerX)
-			jevois.sendSerial(centerX)
-		
-		jevois.sendSerial(" ")		
-
-		
-		#index = len(cntArray)
-		#index = str(index)
-		#jevois.sendSerial(index)
-			
-		#sort cntArray
-		#sortedArray = sorted(cntArray, key=cv2.contourArea)
-				
-		#check if there is more than one contour in sortedArray; then find the two biggest and draw a rectangle around them
-		#arraySize = len(sortedArray)
-		
-		#if arraySize > 1:
-		#	max_index = arraySize - 1
-			
-		#	#First rectangle
-		#	cnt = sortedArray[max_index]
-		#	rotatedRect = cv2.minAreaRect(cnt)
-		#	box = cv2.boxPoints(rotatedRect)
-		#	bigBoxArray	= np.int0(box)
-		#
-		#	cv2.drawContours(outimg,[bigBoxArray],0,(0, 0, 255),2)
-			
-			#Second rectangle
-		#	cnt2 = sortedArray[max_index - 1]
-		#	rotatedRect2 = cv2.minAreaRect(cnt2)
-		#	box2 = cv2.boxPoints(rotatedRect2)
-		#	secondBoxArray = np.int0(box2)
-		
-		#	cv2.drawContours(outimg,[secondBoxArray],0,(0, 0, 255),2)
-			
-			#center coordinates of first rectangle
-		##	centerY = bigBoxArray[0][1]
-			#centerX = bigBoxArray[0][0]
-			#center coordinates of second rectangle
-			#centerY2 = secondBoxArray[0][1]
-			#centerX2 = secondBoxArray[0][0]
-			
-			#angle of robot relative to the target
-		#	yawAngle = (centerX - 155.5)
-		#	yawAngle = yawAngle * 0.203125
-			
-			# slope (or angle) of the target
-		#	targetAngle_A = rotatedRect[2]
-		#	targetAngle_B = rotatedRect2[2]
-
-			#determine which box is on the left and which is on the right
-		#	if centerX < centerX2:
-		#		leftY = centerY
-		#		leftX = centerX
-		#		rightY = centerY2
-		#		rightX = centerX2
-		#		leftAngle = targetAngle_A
-		#		rightAngle = targetAngle_B
-		#	else:
-		#		leftY = centerY2
-		#		leftX = centerX2
-		#		rightY = centerY
-		#		rightX = centerX
-		#		leftAngle = targetAngle_B
-		#		rightAngle = targetAngle_A
-			
-			#dist = math.sqrt((leftX - rightX)**2 + (leftY - rightY)**2)
-			
-			# decide whether we are detecting the correct targets
-			#if leftAngle >= -90 and leftAngle <= -65:
-			#	if rightAngle >= -30 and rightAngle <= -5:
-			#		yesNo = "Yes"
-			#	else:
-			#		yesNo = "No"
-			#else:
-			#	yesNo = "No"
-						
-			
-			#calculate the distance to the target. The values were calculated based on trial and error
-		#	distance = -0.0013216275 * rightY**3 + 0.3319141337 * rightY**2 + -27.11835717 * rightY + 763.1474953
-			
-			#change vals to string to send over serial
-			#yawAngle = str(yawAngle)
-			#distance = str(distance)
-			#leftAngle = str(leftAngle)
-			#rightAngle = str(rightAngle)
-			#dist = str(dist)
-		#	centerY = str(centerY)
-			
-			#send values over serial
-			#jevois.sendSerial(yawAngle)
-			#jevois.sendSerial(distance)
-			#jevois.sendSerial(leftAngle)
-			#jevois.sendSerial(rightAngle)
-			#jevois.sendSerial(yesNo)
-		#	jevois.sendSerial(centerY)
-			
-			#return an image if over usb
 		outimg = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)		
-		#opening2 = cv2.cvtColor(blur, cv2.COLOR_GRAY2BGR)
+		#opening2 = cv2.cvtColor(opening, cv2.COLOR_GRAY2BGR)
 		#outimg = opening2
 		#time.sleep(0.25)
+
+
+		if len(sortedArray) > 0:
+			if self.isRight(sortedArray[0]):
+				sortedArray.pop(0)
+		if len(sortedArray) > 0:
+			if self.isLeft(sortedArray[len(sortedArray) - 1]):
+				sortedArray.pop(len(sortedArray) - 1)
+		
+		if len(sortedArray) < 2 or (len(sortedArray) % 2) != 0:
+			outimg = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+			#jevois.sendSerial("Hello")
+			return outimg
+		
+		#boxColor = (200,50, 180)
+		
+		xArray = []
+		
+		for index in range(0, len(sortedArray), 2):
+			leftRect = cv2.minAreaRect(sortedArray[index])
+			rightRect = cv2.minAreaRect(sortedArray[index + 1])
+			points_A = cv2.boxPoints(leftRect)
+			points_1 = np.int0(points_A)
+			points_B = cv2.boxPoints(leftRect)
+			points_2 = np.int0(points_B)
+			leftX = leftRect[0][0]
+			rightX = rightRect[0][0]
+			cv2.drawContours(outimg, [points_1], 0, boxColor, 2)
+			cv2.drawContours(outimg, [points_2], 0, boxColor, 2)
+			centerX = (leftX + rightX)/2
+			centerX = math.fabs(centerX - 160)
+			xArray.append(centerX)
+		
+		minX = np.argmin(xArray)
+		
+		#jevois.sendSerial("Hello")
+		
+		centerPairLeft = sortedArray[minX * 2]
+		centerPairRight = sortedArray[(minX * 2) + 1]
+		
+		rightRect = cv2.minAreaRect(centerPairRight)
+		leftRect = cv2.minAreaRect(centerPairLeft)
+		
+		leftY = leftRect[0][1]
+		rightY = rightRect[0][1]
+		leftX = leftRect[0][0]
+		rightX = rightRect[0][0]
+		
+		
+		centerY = (leftY + rightY)/2
+		centerX = (leftX + rightX)/2
+		yawAngle = (centerX - 159.5) * 0.203125
+		distance = -0.00002750028278 * centerY **3 + 0.0110106527 * centerY ** 2 -0.7826513252 * centerY + 51.55036834
+		
+		
+		realWorldPointY = (centerX - 159.5)/251
+		realWorldPointY = realWorldPointY * distance
+		
+		
+		#change vals to string to send over serial
+		centerY = str(centerY)
+		distance = str(distance)
+		leftY = str(leftY)
+		rightY = str(rightY)
+		realWorldPointY = str(realWorldPointY)
+		centerX = str(centerX)
+		
+		
+		#send vals over serial
+		jevois.sendSerial("RealWorldPoint: " + realWorldPointY + "centerX: " + centerX)
+		#jevois.sendSerial("Hello World")
 		return outimg
