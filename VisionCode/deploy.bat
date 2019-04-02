95ef743dbc2b11e86ed198358bbb37023d811b4a
EliteHobo37 <elitehobo37@gmail.com>
2019-04-01 20:50:07 -0600

    Update deploy.bat to use more variables

--- VisionCode/deploy.bat
+++ VisionCode/deploy.bat
@@ -2,35 +2,35 @@ taskkill /IM obs64.exe /f
 taskkill /IM putty.exe /f
 taskkill /IM plink.exe /f
 
+set "MODULE_NAME=TapeDetectCody"
+set "COM_PORT=COM14"
 
 cd "C:\Users\Cody1\Desktop\Highlanders\2019-Gravastar\VisionCode"
-start /b plink.exe -load COM14 < mountJevois.txt
+start /b plink.exe -load %COM_PORT% < mountJevois.txt
 
 sleep 10
 
 taskkill /IM plink.exe /f
 
-set "FROM_FILE=TapeDetectCody.py"
-set "TO_FILE=D:\modules\Highlanders\TapeDetectCody\"
+set "FROM_FILE=modules\Highlanders\%MODULE_NAME%\%MODULE_NAME%.py"
+set "TO_FILE=D:\modules\Highlanders\%MODULE_NAME%\"
 cp %FROM_FILE% %TO_FILE%
 
-set "FROM_FILE=script.cfg"
-set "TO_FILE=D:\modules\Highlanders\TapeDetectCody\"
+set "FROM_FILE=modules\Highlanders\%MODULE_NAME%\script.cfg"
 cp %FROM_FILE% %TO_FILE%
 
-set "FROM_FILE=initscript.cfg"
+set "FROM_FILE=config\initscript.cfg"
 set "TO_FILE=D:\config\"
 cp %FROM_FILE% %TO_FILE%
 
-set "FROM_FILE=videomappings.cfg"
-set "TO_FILE=D:\config\"
+set "FROM_FILE=config\videomappings.cfg"
 cp %FROM_FILE% %TO_FILE%
 
 dir %TO_FILE%
 
 sleep 5
 
-start /b plink.exe -load COM14 < restartJevois.txt
+start /b plink.exe -load %COM_PORT% < restartJevois.txt
 taskkill /IM plink.exe /f
 
 sleep 15
